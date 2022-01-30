b26b1308343dd6996edd06792e2523fa1221d97f
neomq <neominqi@hotmail.com>
2022-01-30 06:55:43 +0000

    updated variables and scope

--- 01-variables/scope.js
+++ 01-variables/scope.js
@@ -1,10 +1,14 @@
 const prompt = require('prompt-sync')();
 
-// in javascript {....} forms a scope
-let age = parseInt(prompt("please enter your age: ")) //global scope - any statement not inside curly brackets
+// in JavaScript, { ... } forms a scope
+let age = parseInt(prompt("Please enter your age: "))
 if (age < 67) {
+    console.log("You are",age,'years old');
     console.log("You cannot retire yet");
     console.log("Your CPF money is still frozen");
     let x = 42;
+    if (x >= 42) {
+        console.log(x);
+    }
 }
-console.log(x)
+console.log(x);
--- 01-variables/variables.js
+++ 01-variables/variables.js
@@ -1,38 +1,39 @@
 // Data types in Javascript
-// numbers
-// integers
-// floats
-// strings
-// booleans
-// aka primitive data types
+// - Numbers
+//      > Integers
+//      > floats
+// - Strings
+// - Booleans
+// aka primitive data types (basic data types) or values => values are data
 
 // integer
 42
 
-// strings --> text
+// Strings --> text
 "she sells seashells by the seashore"
 "jack and jill went up the hill"
-'asd1234!'
+'asd1234!' // open with single quote, must end with single quote, same goes for double quote
 
-// boolean values
-// true or false (boolean in javascript is all lower case)
+// Boolean values
+// true or false (boolean in javascript is all lower case, while in python it is uppercase)
 
-// variables
+// Variables
 // variables basically store values
 
 // create js variable
+// use the word 'let'
 let secretOfLife = 42; 
-// let super_long_variable_name = 15;
-// Camel case is preferred in javascript
+// Camel case (above) is preferred in javascript
+// let super_long_variable_name = 15; --- snake case
 // only python uses snake case, other programming languages use Camel case
-// no number as first character, no spaces allowed. '$' and '_' is allowed.
+// no number as first character, no spaces allowed. But '$' and '_' is allowed.
 
 console.log(SecretOfLife);
 // console.log is equivalent to python print()
-// imagine console t be a container that can have many functiond
-// many functions and Log id one of those functions
-// the dot means 'belongs to'
-// think of the . as the 's
+// imagine console to be a container that can have many functions
+// many functions and log is one of those functions
+// the dot means 'belongs to'. (use the log function that belongs to the console)
+// think of the . as the 's'
 // console.log ==> console's.log function
 
 // you can 'let' a variable once (*normally)
