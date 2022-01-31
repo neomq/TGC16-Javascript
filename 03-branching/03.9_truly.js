9b2b7d6f5bec6cccde07c07fb56bc56bf020c59c
neomq <neominqi@hotmail.com>
2022-01-31 09:50:58 +0000

    created truly.js

---
+++ 03-branching/03.9_truly.js
@@ -0,0 +1,13 @@
+// below statements are considered true
+
+if (3) {
+    console.log("3 is found")
+}
+
+if ("lamb") {
+    console.log("mary has a little lamb");
+}
+
+if (["a", "b", "c"]) {
+    console.log("There's an array")
+}
