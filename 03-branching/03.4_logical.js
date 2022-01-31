84d0407ed46b2cb097b34d22acc631c9e121a6f2
neomq <neominqi@hotmail.com>
2022-01-31 09:12:38 +0000

    updated logical.js

--- 03-branching/03.4_logical.js
+++ 03-branching/03.4_logical.js
@@ -1,4 +1,43 @@
-
+// AND operator. In JavaScript we use &&
 let isRaining = false;
 let isHoliday = true;
 let isWeekend = false;
+
+// if it is not raining and it's the weekend, let's go to the beach
+if (isRaining == false && isWeekend == true) {
+    console.log("Go to the beach");
+} else {
+    console.log("Cannot go to the beach");
+}
+
+// => isRaining == false && isWeekend == true
+// => false == false && false == true
+// => true && false
+// => false
+
+if (isRaining == true) {
+    console.log("Bring umbrella")
+}
+// => isRaining == false
+// => false == true
+// => false
+
+if (isRaining) {
+    console.log("Bring umbrella");
+}
+// => isRaining
+// => false
+
+if (isRaining == false) {
+    console.log("Let's go outdoors")
+}
+
+if (!isRaining) {
+    console.log("Let's go outdoors")
+}
+// => !false
+// => true
+
+if (!isRaining && isHoliday) {
+    console.log("Let's go to the beach");
+}
