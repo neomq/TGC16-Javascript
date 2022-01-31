f9edc0fb910529438f41d8fda35dd38b531e152b
neomq <neominqi@hotmail.com>
2022-01-31 08:48:52 +0000

    updated comparison operators

---
+++ 03-branching/03.1_comparison-operators.js
@@ -0,0 +1,15 @@
+console.log("3" == 3)
+// equivalent comparator
+// JS converts string to interger
+// therefore will return true
+
+console.log("3" === 3);
+// strict comparison
+// will return false
+
+console.log("3" != 3)
+// will return false -- JS converts string to integer
+
+console.log("3" !== 3);
+// strict comparison
+// will return true
--- 03-branching/comparison-operators.js
+++
@@ -1,6 +0,0 @@
-console.log("3" == 3)
-
-// strict comparison
-console.log('"3" === 3'), "3" === 3);
-
-// console.log(3 != 3 +> , "")
