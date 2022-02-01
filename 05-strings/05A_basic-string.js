a373a00bb7b81d7f9aba22f8c7f7796612afc812
neomq <neominqi@hotmail.com>
2022-02-01 08:21:10 +0000

    created basic-string.js

---
+++ 05-strings/05A_basic-string.js
@@ -0,0 +1,15 @@
+// strings begin with a ' (single quote) or " (double quote)
+
+let words = "the quick brown fox jumps over the lazy dog";
+let rhyme = 'she sells seashells at the seashore';
+let paragraph = "don't kill me I don't know anything";
+let story = 'she said, "I sells seashells at the seashore"';
+let story2 = 'She said, "I don\'t know anything"';
+let story3 = "He said, \"gong xi fa cai\"";
+
+console.log(story3)
+
+console.log("A\nB"); // next line (enter)
+console.log("Social\tDistancing"); // tab
+console.log("I wish you a merry christmas\nAnd a happy new year\nfor 2022");
+console.log("C:\\documents\\tests"); // escape the '\' symbol
